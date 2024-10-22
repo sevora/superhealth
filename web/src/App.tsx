2b9afcbd84ab2c6ff913d92f06afd727c8812fed
Ralph Louis Gopez <ralph.louis.g@gmail.com>
2024-10-22 14:54:16 +0800

    Add Title and Disclaimer

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -8,6 +8,11 @@ function App() {
   return (
     <div className="w-screen h-screen flex justify-center overflow-x-hidden">
       <div className="p-10 flex flex-col gap-3 w-[700px]">
+        <div>
+          <div className="text-2xl font-medium">Superhealth</div>
+          The models in this web-application are ran directly on your browser. No data is sent to an external server that may track
+          your inputs and results. The model results are not substitute for professional healthcare advice.
+        </div>
         <GeneralInput />
         <HeartDiseaseRisk />
         <MaternalHealthRisk />
