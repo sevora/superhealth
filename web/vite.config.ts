0e19aa6211bd288e9f07eaed0c9cb1055a09a237
Ralph Louis Gopez <ralph.louis.g@gmail.com>
2024-10-24 13:23:47 +0800

    Update Vite Configuration This is so that the onnxruntime-web is properly included when building.

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,12 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  optimizeDeps: {
-    exclude: ['onnxruntime-web'], 
-  },
+  /**
+   * This is for development, in order for it to use the actual onnxruntime-web
+   * which has all the files necessary and not use .vite/deps folder.
+   */
+  optimizeDeps: { exclude: ['onnxruntime-web'] },
   plugins: [
     react(),
+    
+    /**
+     * This is for production, so that every file in onnxruntime-web is included
+     * in the assets of the distribution directory.
+     */
+    viteStaticCopy({ targets: [{ src: './node_modules/onnxruntime-web/dist/*.*', dest: './assets/' }] }),
   ],
 })
