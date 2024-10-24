663ca71cd722d29427a32d49d70093dfaf39dc89
Ralph Louis Gopez <ralph.louis.g@gmail.com>
2024-10-24 12:54:25 +0800

    Add Design on Header Link To emphasize that it is a link.

--- web/src/Header.tsx
+++ web/src/Header.tsx
@@ -7,7 +7,7 @@ function Header() {
         <div>
             <div className="text-2xl font-medium">Superhealth</div>
             The models in this web-application are ran directly on your browser. No data is sent to an external server that may track
-            your inputs and results. The model results are not substitute for professional healthcare advice. Source code is on <a href="https://github.com/sevora/superhealth">Github</a>.
+            your inputs and results. The model results are not substitute for professional healthcare advice. Source code is on <a className="text-blue-600" href="https://github.com/sevora/superhealth">Github</a>.
         </div>
     );
 }
